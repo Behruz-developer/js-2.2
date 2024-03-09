5bc21490864d69f139ac914f1405d5dddbcd7bd7
Behruz-developer <88540036+Behruz-developer@users.noreply.github.com>
2024-03-09 09:02:55 +0500

    js-2.2

---
+++ script.js
@@ -0,0 +1,16 @@
+var age = +prompt('yoshingizni kriting')
+
+if(age <= 18){
+    alert('Yoshsiz. o\'qishingiz kerak')
+}else if(age <= 50){
+    alert('Ishlashingiz  kerak')
+}else if(age <= 59){
+    alert('Yaqinda pensiyaga chiqasiz')
+}else if(age <= 150){
+    alert('Pensionega o\'xshaysiz, hali tirik bo\'lsangiz')
+}else{
+    alert("Nimadir noto'g'ri ketib qoldi")
+}
+
+
+
